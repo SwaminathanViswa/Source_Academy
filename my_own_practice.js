d84c2901c37932d9aeda37eaf1cfbd6bf29e8b72
Source Academy User <SViswa02>
2023-08-28 17:38:39 +0800

    Changes made from Source Academy

--- my_own_practice.js
+++ my_own_practice.js
@@ -1,5 +1,7 @@
 import {circle, square, blank, ribbon, show, beside, beside_frac, stack, stack_frac} from "rune";
 
+
+
 function moony_1(bottom_right) {
     return beside(stack(circle, square), stack(blank, bottom_right));
 }
@@ -23,3 +25,55 @@ function moony(n) {
 }
 
 // show(moony(5));
+
+// function fast_expt(b,n) {
+//     return n === 0
+//         ? 1
+//         : is_even(n)
+//         ? squ(fast_expt(b, n/2))
+//         : b * fast_expt(b, n-1);
+// }
+
+// fast_expt(4,7);
+
+function squ (x) {
+    return x *x;
+}
+
+function is_even(x) {
+    return x % 2 === 0;
+}
+
+
+function fast_expt(b,n) {
+    return iter(b,n,1);
+}
+
+function iter(base, power, product) {
+    return power === 1
+        ? product
+        : is_even(power)
+        ? iter(base, power/2, squ(base) * product)
+        : iter(base, power-1, product * base);
+}
+
+fast_expt(5,3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
