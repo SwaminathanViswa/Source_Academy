c55d9f1ea58a341937b902792628f2f6240f340b
Source Academy User <SViswa02>
2023-09-04 08:41:23 +0800

    Changes made from Source Academy

---
+++ studio4.js
@@ -0,0 +1,22 @@
+// function sum_v2(x) {
+//     function f(y){
+//         return x + y;
+//     }
+//     return f;
+// }
+
+// sum_v2(3)(4);
+
+// function haha(k) {
+//     return k;
+// }
+
+// function hehe(f) {
+//     function anon(hoho) {
+//         return f * hoho;
+//     }
+//     return anon;
+// }
+
+// hehe(5);
+
